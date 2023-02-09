7288495ff072790d7e6b516d259a03d39889299d
Dcruz05 <dcruz11@ucol.mx>
2023-02-08 19:35:43 -0600

    Subiendo practica de clase

---
+++ test.js
@@ -0,0 +1,7 @@
+var celsius = require("celsius-fahrenheit-converter")
+
+let resultado = celsius.celsiusToFahrenheit(50)
+console.log(resultado)
+
+resultado = celsius.fahrenheitToCelcius(142)
+console.log(resultado)
